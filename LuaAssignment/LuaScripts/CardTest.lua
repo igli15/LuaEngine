bc85e4b91e25c3f9285abdbbf7380d40fe22fd3d
Igleer <milaqiigli@gmail.com>
2019-01-29 15:42:31 +0100

    minor.

--- LuaAssignment/LuaScripts/CardTest.lua
+++ LuaAssignment/LuaScripts/CardTest.lua
@@ -12,17 +12,11 @@ end
 
 path = "/Users/igli/Desktop/LuaEngine/LuaAssignment/Assets/"
 
-damage = 30
-
-description = "Heey"
-
-description = (damage > 0 and "Deal " .. damage .. " Damage" or "") .. "\n" .. description;
-
 function InitCards()
 
     cardTemplate.newCard(path.."1.png" ,"Charlie","Deal 3 Damage\nDrawACard",6,3,function() callbacks["drawCard"](); end)
     cardTemplate.newCard(path.."2.png" ,"Grubbytia","Deal 2 Damage",2,2,ability)
-    cardTemplate.newCard(path.."3.png" ,"Drogtia","Deal 6 Damage",7,damage,ability)
+    cardTemplate.newCard(path.."3.png" ,"Drogtia","Deal 6 Damage",7,6,ability)
     cardTemplate.newCard(path.."4.png" ,"Malereed","Deal 2 Damage \n FreezeEnemy",4,2,function() callbacks["freezeOpponent"](); end)
     cardTemplate.newCard(path.."5.png" ,"Tiastomper","Decrease cost of \na random card \nby 1",3,0,function() callbacks["decreaseCostOfACard"](1); end)
     cardTemplate.newCard(path.."6.png" ,"Tiaconda","Decrease cost of \na random card by \nby 3",5,0,function() callbacks["decreaseCostOfACard"](3); end)
@@ -37,7 +31,7 @@ function InitCards()
     cardTemplate.newCard(path.."15.png" ,"Reederion","Deal 3 Damage\nDiscard one card",1,3,function()callbacks ["discardCard"](); end)
     cardTemplate.newCard(path.."16.png" ,"Yoreed","Deal 2 Damage \n FreezeEnemy",4,2,function() callbacks["freezeOpponent"](); end)
     cardTemplate.newCard(path.."17.png" ,"Viserreed","Deal 3 Damage",2,3,ability)
-    cardTemplate.newCard(path.."18.png" ,"Hydratia",description,4,damage,ability)
+    cardTemplate.newCard(path.."18.png" ,"Hydratia","Deal 6 Damage",7,6,ability)
     cardTemplate.newCard(path.."19.png" ,"Suzan","Deal 8 Damage \nFreeze Enemy",8,8,function() callbacks["freezeOpponent"](); end )
     cardTemplate.newCard(path.."20.png" ,"Horsereed","Deal 30 Damage",11,30,ability)
 
